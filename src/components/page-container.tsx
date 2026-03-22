8ab35d2a8154312b64bf0a7f9d1f2d3a61857961
narugakuru <hikari021004@gmail.com>
2026-03-22 13:51:41 +0800

    统一全局宽度为max-w-4xl

--- src/components/page-container.tsx
+++ src/components/page-container.tsx
@@ -5,5 +5,5 @@ import { cn } from "@/lib/utils/utils";
 type PageContainerProps = HTMLAttributes<HTMLDivElement>;
 
 export function PageContainer({ className, ...props }: PageContainerProps) {
-  return <div className={cn("mx-auto w-full max-w-5xl px-4 md:px-6", className)} {...props} />;
+  return <div className={cn("mx-auto w-full max-w-4xl px-4 md:px-6", className)} {...props} />;
 }
